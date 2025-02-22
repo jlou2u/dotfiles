7d1cf921661a3dda06c088003950b6a906cf7760
Justin Lewis <justin.lewis@gmail.com>
2025-02-21 23:53:57 -0500

    working on systemd system service for ltp

--- pkgs/lt/default.nix
+++ pkgs/lt/default.nix
@@ -1,4 +1,12 @@
 { pkgs, ... }:
 
 {
+  packageOverrides =
+    super:
+    let
+      self = super.pkgs;
+    in
+    {
+      myPackage = self.callPackage /home/jlou2u/code/py-lewis-trading { };
+    };
 }
